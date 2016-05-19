aaa930c97b04313a19e29fc1dd5882dcfe997049
Vee <vee@mycostech.com>
2016-05-19 13:00:11 +0700

    Make as service

--- engine.go
+++ engine.go
@@ -4,28 +4,47 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
+	"log"
+	"net"
 	"os"
 	"strings"
-	"time"
 
+	pb "github.com/xenirio/graphite/graph"
 	"github.com/xenirio/graphite/matrix"
+	"google.golang.org/grpc"
 )
+
+const (
+	port = ":50051"
+)
+
+type GraphContext struct{}
+
+var relationMap = make(map[string]map[string]*[]string)
+
+func (s *GraphContext) CreateGraph(origin *pb.Origin, stream pb.Graph_CreateGraphServer) error {
+	edges, lastNodes := matrix.CreateGraph(relationMap, int(origin.Degree), strings.ToUpper(origin.Guid))
+	edges = append(edges, matrix.FindInterconnectedEdges(relationMap, lastNodes)...)
+	for _, e := range edges {
+		edge := pb.Edge{Guid: e.Guid, From: e.From.Guid, To: e.To.Guid}
+		if err := stream.Send(&edge); err != nil {
+			return err
+		}
+	}
+	return nil
+}
 
 func main() {
 	f, _ := os.Open("D:\\Projects\\Golang\\bin\\sample\\relationships.csv")
 	r := csv.NewReader(bufio.NewReader(f))
 	result, _ := r.ReadAll()
-	relationMap := matrix.Create(result)
-	var origin string
-	var degree int
-	fmt.Print("Entity Guid : ")
-	fmt.Scan(&origin)
-	fmt.Print("Degree : ")
-	fmt.Scan(&degree)
-	now1 := time.Now()
-	results := matrix.CreateGraph(relationMap, degree, strings.ToUpper(origin))
-	now2 := time.Now()
-	fmt.Println(results)
-	fmt.Printf("Total : %d\n", len(results))
-	fmt.Printf("Time : %s\n", now2.Sub(now1))
+	relationMap = matrix.Create(result)
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGraphServer(s, &GraphContext{})
+	s.Serve(lis)
+	fmt.Println("service searted")
 }
--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,14 +1,14 @@
 package matrix
 
 type Node struct {
-	guid   string
-	degree int
+	Guid   string
+	Degree int
 }
 
 type Edge struct {
-	guid string
-	from Node
-	to   Node
+	Guid string
+	From Node
+	To   Node
 }
 
 func Create(content [][]string) map[string]map[string]*[]string {
@@ -37,35 +37,35 @@ func Create(content [][]string) map[string]map[string]*[]string {
 	return table
 }
 
-func CreateGraph(content map[string]map[string]*[]string, degree int, guid string) []Edge {
+func CreateGraph(content map[string]map[string]*[]string, degree int, guid string) ([]Edge, map[string]Node) {
 	nodes := make(map[string]Node)
 	edges := []Edge{}
 	queue := make([]Node, 0)
 	inQueue := make(map[string]bool)
 	visited := make(map[string]bool)
-	lastDegreeNodes := make(map[string]bool)
-	nodes[guid] = Node{guid: guid, degree: 0}
+	lastDegreeNodes := make(map[string]Node)
+	nodes[guid] = Node{Guid: guid, Degree: 0}
 	queue = append(queue, nodes[guid])
 	for len(queue) > 0 {
 		elem := queue[0]
 		queue = queue[1:]
-		visited[elem.guid] = true
-		nextDegree := elem.degree + 1
+		visited[elem.Guid] = true
+		nextDegree := elem.Degree + 1
 
-		for k, _ := range content[elem.guid] {
+		for k, _ := range content[elem.Guid] {
 			if _, ok := nodes[k]; !ok {
-				nodes[k] = Node{guid: k}
+				nodes[k] = Node{Guid: k}
 			}
 			if _, ok := visited[k]; !ok {
-				for i := range *content[elem.guid][k] {
-					edges = append(edges, Edge{guid: (*content[elem.guid][k])[i], from: nodes[elem.guid], to: nodes[k]})
+				for i := range *content[elem.Guid][k] {
+					edges = append(edges, Edge{Guid: (*content[elem.Guid][k])[i], From: nodes[elem.Guid], To: nodes[k]})
 				}
 				if _, ok := inQueue[k]; !ok {
 					if nextDegree == degree {
-						lastDegreeNodes[k] = true
+						lastDegreeNodes[k] = nodes[k]
 					} else {
 						node := nodes[k]
-						node.degree = nextDegree
+						node.Degree = nextDegree
 						queue = append(queue, node)
 						inQueue[k] = true
 					}
@@ -73,19 +73,21 @@ func CreateGraph(content map[string]map[string]*[]string, degree int, guid strin
 			}
 		}
 	}
-	tmpLastDegreeNodes := lastDegreeNodes
-	for keyLastNode, _ := range tmpLastDegreeNodes {
+	return edges, lastDegreeNodes
+}
+
+func FindInterconnectedEdges(content map[string]map[string]*[]string, nodes map[string]Node) []Edge {
+	edges := []Edge{}
+	tmpNodes := nodes
+	for keyLastNode, _ := range tmpNodes {
 		for k, _ := range content[keyLastNode] {
-			if _, ok := nodes[k]; !ok {
-				nodes[k] = Node{guid: k}
-			}
-			if _, ok := lastDegreeNodes[k]; ok {
+			if _, ok := nodes[k]; ok {
 				for i := range *content[keyLastNode][k] {
-					edges = append(edges, Edge{guid: (*content[keyLastNode][k])[i], from: nodes[keyLastNode], to: nodes[k]})
+					edges = append(edges, Edge{Guid: (*content[keyLastNode][k])[i], From: nodes[keyLastNode], To: nodes[k]})
 				}
 			}
 		}
-		delete(lastDegreeNodes, keyLastNode)
+		delete(nodes, keyLastNode)
 	}
 	return edges
 }
